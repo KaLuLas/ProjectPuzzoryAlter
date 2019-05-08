0207d64ffa4cd4200aa9653abfeafed6f7294c0b
KaLuLas <499485532@qq.com>
2019-05-08 22:31:50 +0800

    user.py func login & register

--- puzztory/user.py
+++ puzztory/user.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 from django.http import HttpResponseRedirect
+from django.db import IntegrityError
 
 
 def register(request):
@@ -21,16 +22,16 @@ def register(request):
         pwd = request.POST['p']
         # successfully create new user
         register_dict = {}
-        user = User.objects.create_user(
-            username=user_email, email=user_email, password=pwd)
-        if user is None:
+        try:
+            user = User.objects.create_user(
+                username=user_email, email=user_email, password=pwd)
+        except IntegrityError:
             register_dict['emailExistedAlert'] = "邮箱地址已被注册"
             return render(request, 'register.html', register_dict)
-        else:
-            user.save()
-            login_dict['useremail'] = user_email
-            user_record = Usertable(useremail=user_email, username=user_name)
-            user_record.save()
+        user.save()
+        login_dict['useremail'] = user_email
+        user_record = Usertable(useremail=user_email, username=user_name)
+        user_record.save()
     return render(request, 'login.html', login_dict)
 
 
@@ -48,7 +49,7 @@ def Login(request):
             Usertable.objects.get(useremail=user_email)
         except Usertable.DoesNotExist:
             login_dict['emailNotExistedAlert'] = "该用户不存在"
-            return render(request, "login.html", login_dict) 
+            return render(request, "login.html", login_dict)
         user = authenticate(username=user_email, password=pwd)
         if user is None:
             login_dict['passwordIncorrect'] = "密码错误"
@@ -56,5 +57,5 @@ def Login(request):
         else:
             login(request, user)
             # return render(request, "index.html")
-            return HttpResponseRedirect('')
+            return HttpResponseRedirect("index")
     return render(request, "login.html", login_dict)
