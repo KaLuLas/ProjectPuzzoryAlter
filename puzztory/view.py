e3d8be9d1c5dd0681c53d49dd758bc44dfabb4bb
KaLuLas <499485532@qq.com>
2019-05-21 19:48:12 +0800

    back to the start

--- puzztory/view.py
+++ puzztory/view.py
@@ -24,10 +24,7 @@ def homepage(request):
     page_obj = paginator.get_page(page)
     index_dict['paginator'] = paginator
     index_dict['page_obj'] = page_obj
-    if paginator.num_pages() > 1:
-        index_dict['is_paginated'] = True
-    else:
-        index_dict['is_paginated'] = False
+    index_dict['is_paginated'] = True
     return render(request, 'index.html', index_dict)
 
 
