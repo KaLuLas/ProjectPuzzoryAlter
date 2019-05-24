ec69f8d919a8a07b9356953e60dc975205db54b5
Alex <32681448+dontknow9179@users.noreply.github.com>
2019-05-24 20:53:39 +0800

    add ... for lfcontent

--- puzztory/view.py
+++ puzztory/view.py
@@ -217,7 +217,7 @@ def lock(request):
         last_fragment = Fragment.objects.filter(
             storyid=request_id).order_by('-createtime')[0]
         if len(last_fragment.content) > 20:
-            lfcontent_text = last_fragment.content[-20:]
+            lfcontent_text = '...' + last_fragment.content[-20:]
         else:
             lfcontent_text = last_fragment.content
         ret_dict['lfcontent'] = lfcontent_text
