69717059b4e36ddcddf58d3f3a9fa42a89284264
KaLuLas <499485532@qq.com>
2019-05-25 17:43:06 +0800

    ???

--- puzztory/view.py
+++ puzztory/view.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-# from PuzzModel.models import Fragment, UserExtension, Story
+from PuzzModel.models import Fragment, UserExtension, Story
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.core.paginator import Paginator
